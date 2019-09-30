9dc5d52e94360ccf5f469eace11ec163a86bba46
Juan Jesús Gutiérrez Romero <necrobone@hotmail.com>
2019-09-30 11:47:20 +0200

    Update migration username

--- app/DoctrineMigrations/Version20190609134544.php
+++ app/DoctrineMigrations/Version20190609134544.php
@@ -19,7 +19,7 @@ class Version20190609134544 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, api_key VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), UNIQUE INDEX UNIQ_8D93D649C912ED9D (api_key), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('INSERT INTO user (username, api_key) VALUES ("rindus", "1eb6ea66-a6b9-466c-9692-c1e459745262")');
+        $this->addSql('INSERT INTO user (username, api_key) VALUES ("symfony", "1eb6ea66-a6b9-466c-9692-c1e459745262")');
     }
 
     /**
